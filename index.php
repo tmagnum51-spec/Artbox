a18b500aefdaab3958ce29606c068f088f917726
Thomas <tmagnum51@gmail.com>
2026-02-15 17:29:31 +0100

    Ajout de mes fichiers Artbox

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+    require 'header.php';
+    require 'oeuvres.php';
 ?>
-Something is wrong with the XAMPP installation :-(
+<div id="liste-oeuvres">
+    <?php foreach($oeuvres as $oeuvre): ?>
+        <article class="oeuvre">
+            <a href="oeuvre.php?id=<?= $oeuvre['id'] ?>">
+                <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+                <h2><?= $oeuvre['titre'] ?></h2>
+                <p class="description"><?= $oeuvre['artiste'] ?></p>
+            </a>
+        </article>
+    <?php endforeach; ?>
+</div>
+<?php require 'footer.php'; ?>
