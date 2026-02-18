3bd8360db06292b809231bd1a2ea2ccfbe39478c
Thomas <tmagnum51@gmail.com>
2026-02-18 16:10:32 +0100

    :adhesive_bandage: BDD mise a jour du fichier export bdd avec les oeuvre ajoutée lors des tests de fomulaire

--- oeuvres.sql
+++ oeuvres.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 16 fév. 2026 à 10:15
+-- Généré le : mer. 18 fév. 2026 à 16:03
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -27,7 +27,6 @@ SET time_zone = "+00:00";
 -- Structure de la table `oeuvres`
 --
 
-DROP TABLE IF EXISTS `oeuvres`;
 CREATE TABLE `oeuvres` (
   `id_oeuvre` int(11) NOT NULL,
   `titre` varchar(128) NOT NULL,
@@ -44,7 +43,7 @@ INSERT INTO `oeuvres` (`id_oeuvre`, `titre`, `description`, `artiste`, `image`) 
 (1, 'Dodomu', 'Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu (\"domicile\" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu\\\'elle partage, ayant elle-même dû quitter son foyer. L\\\'œuvre évoque le drapeau liquéfié d\\\'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d\\\'émotion est le symbole d\\\'un événement qui marquera l\\\'Histoire. Cette peinture à l\\\'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.', 'Mia Tozerski', 'img/clark-van-der-beken.png'),
 (2, 'Aashaaheen Baadal', 'Sur cette oeuvre conceptuelle à la fois organique, minérale et liquide, Anaisha Devi nous transporte dans un nuage noir envoûtant. Un sombre tableau qui, par son verni éclatant, rayonne tel un marbre poli. Une oeuvre à la cohérence transcendantale, exécutée à la perfection', 'Anaisha Devi', 'img/pawel-czerwinski-3.png'),
 (3, 'Nightlife Traffic', 'Quisque accumsan ultrices ligula vestibulum posuere. Aliquam feugiat ligula eget massa blandit condimentum. Morbi volutpat erat luctus suscipit pellentesque. Quisque cursus tempor nibh at sollicitudin. Sed blandit libero velit. Etiam tincidunt facilisis mollis. Ut mollis nunc sit amet lacinia luctus. Suspendisse volutpat enim semper arcu rutrum, et iaculis risus interdum. Duis at libero.', 'Andrew Forsythe', 'img/dan-cristian-padure.png'),
-(4, 'Le refuge de l\\\'Havre', 'Nam tempus neque nec felis venenatis auctor. Nam velit risus, lobortis eu quam non, interdum efficitur nibh. Phasellus a augue ac orci lacinia mattis et vel lectus. Sed nec tellus urna. Donec at turpis turpis. Cras quam tellus, imperdiet vitae finibus id, varius quis felis. Maecenas blandit eleifend risus, vel hendrerit erat dignissim id. Nullam at laoreet nibh. Nulla gravida varius sollicitudin. Etiam non aliquam diam, tempor varius sapien. Aenean et velit eu nisi lobortis massa nunc.', 'Simon Pelletier', 'img/steve-johnson-5.png'),
+(4, 'Le refuge de l\'Havre', 'Nam tempus neque nec felis venenatis auctor. Nam velit risus, lobortis eu quam non, interdum efficitur nibh. Phasellus a augue ac orci lacinia mattis et vel lectus. Sed nec tellus urna. Donec at turpis turpis. Cras quam tellus, imperdiet vitae finibus id, varius quis felis. Maecenas blandit eleifend risus, vel hendrerit erat dignissim id. Nullam at laoreet nibh. Nulla gravida varius sollicitudin. Etiam non aliquam diam, tempor varius sapien. Aenean et velit eu nisi lobortis massa nunc.', 'Simon Pelletier', 'img/steve-johnson-5.png'),
 (5, 'Red Washover', 'Nunc euismod ullamcorper tortor, id efficitur ante interdum in. Integer eu condimentum nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras viverra suscipit feugiat. Mauris vehicula luctus tellus, eu hendrerit libero laoreet ut. In tristique vehicula nisl in tempus. Morbi tempus aliquet gravida. In eget est congue, rhoncus sapien at, cursus metus.', 'Kit Van Der Borght', 'img/steve-johnson.png'),
 (6, 'Chromatics', 'Vivamus commodo non libero at hendrerit. In lacinia dui sit amet pellentesque iaculis. Donec at ultricies sem porttitor.', 'Jean-Michel Delatronchette', 'img/pawel-czerwinski.png'),
 (7, 'Digital Negative', 'Integer in nisl posuere, pulvinar ex ac, tincidunt risus. Nullam vel lorem et leo dignissim accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempor, magna non consectetur dapibus, est libero iaculis lacus, eget semper turpis orci vitae felis. Fusce eget molestie.', 'Hamish McKee', 'img/jazmin-quaynor.png'),
@@ -55,7 +54,10 @@ INSERT INTO `oeuvres` (`id_oeuvre`, `titre`, `description`, `artiste`, `image`) 
 (12, 'La Galaxia Gialla', 'Mauris maximus, orci sollicitudin ultrices elementum, tellus neque feugiat leo, quis lobortis purus neque vel lectus. Ut sagittis eros id lectus porttitor tincidunt. Donec scelerisque diam nec felis egestas, eget finibus ante porttitor. Sed malesuada sapien ut semper accumsan. Duis tristique dui vel egestas porttitor. Nunc tristique dapibus orci a amet.', 'Eduardo Tancredi', 'img/fly-d.png'),
 (13, 'Puffy Amalgamate', 'Donec semper a massa quis congue. In malesuada lorem ligula, ut posuere magna pulvinar in. Proin vitae enim gravida, commodo odio.', 'Sandro De Blasi', 'img/orfeas-green.png'),
 (14, 'Mirage', 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam iaculis lorem ac ex tristique egestas et nec sapien. Donec tincidunt id erat sit amet tempus. Nullam vel molestie dui. Duis a neque massa. Vivamus quis ornare lacus. Nullam eleifend condimentum egestas. Vivamus magna purus, fermentum mollis mauris sed, consectetur interdum libero. Duis interdum tortor tellus, eget sollicitudin quis.', 'Stéphanie Kaiser', 'img/steve-johnson-4.png'),
-(15, 'Blaue Gelbe Muster', 'Curabitur dui odio, porta vel tempor sed, consectetur vitae mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Orci varius natoque penatibus nec.', 'Adelheid Von Schreiber', 'img/steve-johnson-3.png');
+(15, 'Blaue Gelbe Muster', 'Curabitur dui odio, porta vel tempor sed, consectetur vitae mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Orci varius natoque penatibus nec.', 'Adelheid Von Schreiber', 'img/steve-johnson-3.png'),
+(16, 'Tête À Toto', ' Technique mixte sur toile et plaque de plexiglas .', 'David Ferreira', 'https://bel-arti.com/wp-content/uploads/2025/07/tete-a-Toto--scaled.jpg'),
+(24, 'M002', 'Techniques mixtes et acrylique , toile encadrée', 'Sophie Sanguinetti', 'https://bel-arti.com/wp-content/uploads/2025/04/IMG_3251-scaled.jpg'),
+(28, 'Le Dandy', 'Acrylique sur bois', 'Jean-Michel Cordier', 'https://bel-arti.com/wp-content/uploads/2023/04/Le-Dandy.jpeg');
 
 --
 -- Index pour les tables déchargées
@@ -75,7 +77,7 @@ ALTER TABLE `oeuvres`
 -- AUTO_INCREMENT pour la table `oeuvres`
 --
 ALTER TABLE `oeuvres`
-  MODIFY `id_oeuvre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_oeuvre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
